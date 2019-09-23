b3ca5feb2827563181d0578fcc31fb20729bddda
stav <sst46@nau.edu>
2019-09-23 13:20:01 -0700

    working game.js and index.html on local

--- game.js
+++ game.js
@@ -1,66 +1,49 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x000033});
+var renderer = PIXI.autoDetectRenderer(400,400,{backgroundColor: 0x000000});
 gameport.appendChild(renderer.view);
 
+
 var stage = new PIXI.Container();
+var texture = PIXI.Texture.fromImage("slime_sprite.png");
 
-var planet_a = new PIXI.Sprite(PIXI.Texture.fromImage("planet_a.png"));
-var planet_b = new PIXI.Sprite(PIXI.Texture.fromImage("planet_b.png"));
-var moon_a = new PIXI.Sprite(PIXI.Texture.fromImage("moon_a.png"));
-var sun = new PIXI.Sprite(PIXI.Texture.fromImage("sun.png"));
+var sprite = new PIXI.Sprite(texture);
 
-// Create the Solar system and add it to the stage
-var solar_system = new PIXI.Container();
-solar_system.position.x = 200;
-solar_system.position.y = 200;
-stage.addChild(solar_system);
+sprite.anchor.x = 0.5;
+sprite.anchor.y = 0.5;
 
-// Add the Sun to the solar system
-solar_system.addChild(sun);
-sun.anchor.x = 0.5;
-sun.anchor.y = 0.5;
-sun.position.x = 0;
-sun.position.y = 0;
+sprite.position.x = 200;
+sprite.position.y = 200;
 
-// Create the first planet system and add it to the stage
-var planet_a_system = new PIXI.Container();
-planet_a_system.position.x = 100;
-planet_a_system.position.y = 75;
-solar_system.addChild(planet_a_system);
+stage.addChild(sprite);
 
-// Add planet_a to the planet_a_system
-planet_a_system.addChild(planet_a);
-planet_a.anchor.x = 0.5;
-planet_a.anchor.y = 0.5;
-planet_a.position.x = 0;
-planet_a.position.y = 0;
+function keydownEventHandler(e){
 
-// Add a moon to planet_a
-planet_a_system.addChild(moon_a);
-moon_a.anchor.x = 0.5;
-moon_a.anchor.y = 0.5;
-moon_a.position.x = 40;
-moon_a.position.y = 30;
+   if(e.keyCode == 87) //w key
+   {
+      sprite.position.y -=10;
+   }
+   if(e.keyCode == 83) //s key
+   {
+      sprite.position.y +=10;
+   }
+   if(e.keyCode == 65) //w key
+   {
+      sprite.position.x -=10;
+   }
+   if(e.keyCode == 68) //w key
+   {
+      sprite.position.x +=10;
+   }
+}
 
-// Add planet_b to the solar system
-solar_system.addChild(planet_b);
-planet_b.anchor.x = 0.5;
-planet_b.anchor.y = 0.5;
-planet_b.position.x = 35;
-planet_b.position.y = -45;
+document.addEventListener('keydown', keydownEventHandler);
 
-function animate() {
-  requestAnimationFrame(animate);
 
-  // Rotate the planets
-  planet_a_system.rotation += 0.025;
-  planet_b.rotation += 0.05;
-  moon_a.rotation += 0.05;
-
-  // Rotate the Solar system
-  solar_system.rotation += 0.01;
-
-  renderer.render(stage);
+function animate()
+{
+   requestAnimationFrame(animate);
+   //sprite.rotation +=0.1;
+   renderer.render(stage);
 }
 animate();
